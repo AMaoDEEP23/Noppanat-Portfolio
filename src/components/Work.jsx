0ab9ecda8669f94f40540d3bfbf2652aafee9bf7
AMaoDEEP23 <151827609+AMaoDEEP23@users.noreply.github.com>
2024-04-09 13:08:44 +0700

    update11

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -153,7 +153,7 @@ const Work = () => {
                    {/* Hover effects */}
                     <div className='opacity-0 group-hover:opacity-100'>
                         <span className='text-2xl font bold text-white tracking-wider'>
-                            React Burger
+                            BookingApp On Processing
                         </span>
                         <div className='pt-8 text-center'>
                             <a href="https://booking-nv82tq62x-noppanats-projects.vercel.app/"
